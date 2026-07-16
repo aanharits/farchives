7b481be5bf054b99feefba9cabcc8d4fa5f17785
aanharits <farhan.harits05@gmail.com>
2026-07-17 01:37:42 +0700

    refactor: update navigation interactions and add new cat gallery component with data source

---
+++ src/data/cats.ts
@@ -0,0 +1,67 @@
+export interface Cat {
+  name: string;
+  ascii: string;
+}
+
+export const cats: Cat[] = [
+  {
+    name: "Poni",
+    ascii: ` /\\_/\\
+( o.o )
+ > ^ <`,
+  },
+  {
+    name: "Kumis",
+    ascii: ` /\\_/\\
+( -.- )
+ ~ v ~`,
+  },
+  {
+    name: "Putih",
+    ascii: ` /\\_/\\
+( O.O )
+ /| |\\`,
+  },
+  {
+    name: "Momo",
+    ascii: ` /\\_/\\
+( >w< )
+ || ||`,
+  },
+  {
+    name: "Mimi",
+    ascii: ` /\\_/\\
+( @.@ )
+ |_ _|`,
+  },
+  {
+    name: "Mumu",
+    ascii: `  /\\_/\\
+ ( ^.^ )
+  \\| |/`,
+  },
+  {
+    name: "Moli",
+    ascii: ` /\\_/\\
+( o.O )
+ |/ \\|`,
+  },
+  {
+    name: "Yoma",
+    ascii: ` /\\_/\\
+( =.= )
+ )   (`,
+  },
+  {
+    name: "Yomi",
+    ascii: ` /\\_/\\
+( ~.~ )
+ [___]`,
+  },
+  {
+    name: "Usro",
+    ascii: ` /\\_/\\
+( *.* )
+ |   |~`,
+  },
+];
